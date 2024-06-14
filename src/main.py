199f1c84f6d674bb2ff8b3c408508681951cde28
Rahul <80764087+rahulsk88@users.noreply.github.com>
2024-06-14 09:29:16 -0400

    Used Torch + Vectorized all operations + Source Control

--- src/main.py
+++ src/main.py
@@ -1,28 +1,24 @@
-import numpy as np
+import torch
 import matplotlib.pyplot as plt
 
-## TODO: Add additional models and integrate properly
-## TODO: Add abstract methods when these models are made.  
-
-class Admission_MDP:
-  def __init__(self, goal, admission_decay, time=False, num_classes=3,
-                 acceptance_probabilities=np.array([0.231]), max_time=10,
-                 patience=4, new_students=np.array([3]), epsilon=0.0, alpha=0.1, gamma=0.9, q_table = {}):
+class Admission_MDP_vectorised:   ## Not sure whether this is correct
+  def __init__(self, goal, admission_decay = torch.tensor([0.31, 0.12]), batch_size = 128, num_classes=2,
+               acceptance_probabilities=torch.tensor([0.631, .02]), max_time=10,
+               patience=4, new_students=torch.tensor([10, 7]), epsilon=0.3, alpha=0.1,
+               gamma=0.9, q_table = {}):
     ## Fixed elements
-    self.goal = goal
+    self.goal = torch.tensor([goal])
     self.num_classes = num_classes
     self.acceptance_probabilities = acceptance_probabilities
     self.admission_decay = admission_decay
     self.max_time = max_time
     self.patience= patience
-    self.delayed_acceptance_probabilities = self._admit_decay()
-    self.new_students = new_students
-
-
-    ## Learning params:
     self.epsilon = epsilon
     self.alpha = alpha
     self.gamma = gamma
+    self.batch = batch_size
+    self.delayed_acceptance_probabilities = self._admit_decay()
+    self.new_students = new_students
 
     ## Initiallise Start State
     self.state = self._start_state()
@@ -32,19 +28,23 @@ class Admission_MDP:
 
   def _start_state(self):
         ## State Inits elements
-        pending_offers = np.zeros(self.num_classes, dtype=int) + self.new_students
-        sent_offers_matrix = np.zeros((self.patience, self.num_classes), dtype=int)
-        total_accepted = 0
+        pending_offers = torch.zeros((self.batch, self.num_classes), dtype=int) + self.new_students  ## (batch_size, num_classes)
+        sent_offers_matrix = torch.zeros((self.batch, self.num_classes, self.patience), dtype=int)   ## (batch_size, num_classes, patience)
+        total_accepted = torch.zeros(self.batch, dtype = int)                                                     ## (batch_size)
 
-        return (pending_offers, sent_offers_matrix, total_accepted)
+        return pending_offers, sent_offers_matrix, total_accepted  ## Can this not be an array? no because the size of each dimension is differnt
 
   def _admit_decay(self): ## Weak intrenal method to populate the delayed_acceptance_probabilities
-        delayed_acceptance_probabilities = np.zeros((self.patience, self.num_classes))
+        delayed_acceptance_probabilities = torch.zeros((self.num_classes, self.patience))
         for i in range(self.patience):
             if i == 0:
-                delayed_acceptance_probabilities[i, :] = self.acceptance_probabilities
+                delayed_acceptance_probabilities[:, i] = self.acceptance_probabilities
             else:
-                delayed_acceptance_probabilities[i, :] = np.maximum(0, delayed_acceptance_probabilities[i-1, :] - self.admission_decay)
+                delayed_acceptance_probabilities[:, i] =  delayed_acceptance_probabilities[:, i-1] - self.admission_decay
+        
+        delayed_acceptance_probabilities = torch.relu(delayed_acceptance_probabilities)
+        delayed_acceptance_probabilities = delayed_acceptance_probabilities.expand(self.batch, self.num_classes, self.patience)
+
         
         return delayed_acceptance_probabilities
 
@@ -52,125 +52,102 @@ class Admission_MDP:
 
   def _reward(self, total_Accepted): ## Internal method for reward calculation
     ## L2 penalty of the difference between accepted goal and another penalty if no offers are sent.
-    reward = -abs(self.goal - total_Accepted)
+    reward = -abs(self.goal - total_Accepted)  ## Broadcasting should take care of this
     return reward
 
+  def _new_student_generator(self):
+    pass
+
+
   def choose_action(self):
-        pending_offers, _, _ = self.state
+      pending_offers, _, _ = self.state
+      random_numbers = torch.rand(self.batch)
 
-        if np.random.rand() < self.epsilon:
-            ## Exploration: choose a random action
-            action = tuple(np.random.randint(0, pending + 1) for pending in pending_offers)
+      actions = torch.zeros((self.batch, self.num_classes), dtype=int)
+      explore = random_numbers < self.epsilon
 
-        else:
-            ## Exploitation: choose the best action based on Q-values
-            state_key = str(self.state)
-            if state_key in self.q_table:
-                action = tuple(max(self.q_table[state_key], key=self.q_table[state_key].get))
-            else:
-                action = tuple(np.random.randint(0, pending + 1, dtype=np.int64()) for pending in pending_offers)
+      for i in range(self.num_classes):
+          actions[:, i] = torch.where(
+              explore,
+              torch.randint(low=0, high=pending_offers[:, i].max().item() + 1, size=(self.batch,)),
+              torch.tensor([self.q_table.get(str(tuple(pending_offers[j].tolist())), {}).get((i,), 0) for j in range(self.batch)])
+          )
 
-        return action
+      return actions
 
-  def transition(self, action):  ## There is an error here with
-                                 ## double counting.
-    ## Unpacking the state:
+  def transition(self, actions):
+      pending_offers, sent_offers_matrix, total_accepted = self.state
+      pending_offers -= actions
+      pending_offers = torch.relu(pending_offers)  # Ensure no negative values
+
+      sent_offers_matrix = torch.roll(sent_offers_matrix, shifts=1, dims=1)
+      sent_offers_matrix[:, :, 0] = actions
+
+      generator = torch.distributions.binomial.Binomial(sent_offers_matrix, self.delayed_acceptance_probabilities)
+      new_admits = generator.sample()
+
+      total_accepted += new_admits.sum((1, 2)).int()
+      sent_offers_matrix -= new_admits.int()
+
+      pending_offers += self.new_students
+
+      self.state = (pending_offers, sent_offers_matrix, total_accepted)
+      return self.state
+
+  def update_q_vals(self, actions, rewards, next_states):
     pending_offers, sent_offers_matrix, total_accepted = self.state
+    for i in range(self.batch):
+        state_key = f"{pending_offers[i].tolist()}-{sent_offers_matrix[i].view(-1).tolist()}-{total_accepted[i].item()}"
+        action_key = tuple(actions[i].tolist())
+        next_state_key = f"{next_states[0][i].tolist()}-{next_states[1][i].view(-1).tolist()}-{next_states[2][i].item()}"
 
-    action = np.array(action, dtype=int)
-    ## Update pending_offers
-    pending_offers -= action
+        # Initialize dictionaries for new states and actions
+        if state_key not in self.q_table:
+            self.q_table[state_key] = {}
+        if next_state_key not in self.q_table:
+            self.q_table[next_state_key] = {}
 
-    pending_offers = (np.abs(pending_offers) + pending_offers) / 2
+        # Default to 0 if not already present
+        current_q_value = self.q_table[state_key].get(action_key, 0)
+        best_next_q_value = max(self.q_table[next_state_key].values(), default=0)
 
-    ## Then we age all pending offers.
-    sent_offers_matrix = np.roll(sent_offers_matrix, shift = 1, axis=0)
-
-    ## Setting age current offers to 0
-    sent_offers_matrix[0, :] = 0
-
-    ## Then we add new offers to the pile
-    sent_offers_matrix[0, :] = action
-
-    ## Then we determine the transition after the aging to prevent double counting
-    sent_offers_matrix = sent_offers_matrix.astype(int)
-    new_admits = np.random.binomial(sent_offers_matrix, self.delayed_acceptance_probabilities)
-
-    ## Update Total Acccepted students for reward calculation
-    total_accepted += new_admits.sum()
-
-    ## Remove the students from the pending_students:
-    sent_offers_matrix -= new_admits
-
-    ## Add new students to pending_offers
-    pending_offers += self.new_students
-
-    ## Update New Students
-    new_state = (pending_offers, sent_offers_matrix, total_accepted)
-    self.state = new_state
-    return new_state
-
-  def update_q_vals(self, action, reward, next_state):
-
-    """
-    Takes in the state, action, reward and the next_state do the following:
-    Q(s, a) = Q(s, a) + alpha * (reward + gamma * max(Q(s', a')) - Q(s, a)) 
-    """
-    state_key = str(self.state)
-    action_key = action  ## Might not work for multiples right? 
-    next_state_key = str(next_state)
-
-    ## Q Table Logic 
-    if state_key not in self.q_table:  ## If the current state is not in the table, we add it
-        self.q_table[state_key] = {}   ## We create a specific dictioanry for this 
-    if action_key not in self.q_table[state_key]: ## We then check whether that particular action has been taken before. If it has yay, else, we add it
-        self.q_table[state_key][action_key] = 0 ## We set it to 0 because we do the calculations next anyway
-    if next_state_key not in self.q_table: ## 
-        self.q_table[next_state_key] = {}
-
-    best_next_action = max(self.q_table[next_state_key], key=self.q_table[next_state_key].get, default=(0,) * self.num_classes)
-    best_next_q_value = self.q_table[next_state_key].get(str(best_next_action), 0)
-
-    self.q_table[state_key][action_key] += self.alpha * (reward + self.gamma * best_next_q_value - self.q_table[state_key][action_key])  ## Double check the math but I think it is correct
+        # Update the Q-value using the learning rate
+        updated_q_value = current_q_value + self.alpha * (rewards[i] + self.gamma * best_next_q_value - current_q_value)
+        self.q_table[state_key][action_key] = updated_q_value
 
 
   def step(self):
-    ## Choosing Action
-    action = self.choose_action()
-    ## Transition to the next state action is chosen within transition
-    new_state = self.transition(action)
+      actions = self.choose_action()
+      new_states = self.transition(actions)
 
-    ## Reward calc
-    _, _, total_accepted = new_state
-    reward = self._reward(total_accepted)
+      _, _, total_accepted = new_states
+      rewards = self._reward(total_accepted)
 
-    ## Update Rule
-    self.update_q_vals(action, reward, new_state)
+      self.update_q_vals(actions, rewards, new_states)
 
-    self.max_time -= 1
-    return new_state, reward, action
+      self.max_time -= 1
+      return new_states, rewards, actions
 
-  def run(self, epochs = 1000):
-    ## Plotting Stuff
-    rewards_per_epochs = []
-    ## Training stuff
-    for epoch in range(epochs):
 
-        self.state = self._start_state()
-        self.max_time = 10
-        total_reward = 0
+  def run(self, epochs=1000):
+      rewards_per_epoch = []
+      try:
+        for epoch in range(epochs):
+            self.state = self._start_state()
+            self.max_time = 10
+            total_reward = 0
 
-        while self.max_time > 0:
-          _, reward, _ = self.step()
-          total_reward += reward
+            while self.max_time > 0:
+                _, rewards, _ = self.step()
+                total_reward += rewards.sum().item()
 
-        rewards_per_epochs += [total_reward]
-        #print(f"Epoch Number {epoch} has ended")
-    plt.plot(rewards_per_epochs)
-    plt.xlabel('Epoch')
-    plt.ylabel('Reward')
-    plt.title('Reward per Epoch')
-    plt.show()
+            rewards_per_epoch.append(total_reward/self.batch)
+      finally: 
+        plt.plot(rewards_per_epoch)
+        plt.xlabel('Epoch')
+        plt.ylabel('Reward')
+        plt.title('Reward per Epoch')
+        plt.show()
 
-    ## Plotting
-    return self.q_table
+        return self.q_table
+
